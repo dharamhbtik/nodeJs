7ffce61296b1f9f6aee8c49f13342cc4f74d57f2
Dharmendra Kumar <dharmendra.hbti@gmail.com>
2022-04-16 10:45:07 +0530

    NodeMon update

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ console.log("Hello Node JS");
 console.log(`hello ${userData.name}, I will call you on ${userData.mobile}`);
 
 
-const items = [1,[2,[3,[4]]]];
+const items = [1,[2,[3,[4]]],5];
 const newItems = _.flattenDeep(items);
 console.log(newItems);
